2fac326428f3d6add589b7a98fb0f18aef53e919
rhiltz22 <167123529+rhiltz22@users.noreply.github.com>
2024-04-22 13:32:52 -0500

    Update ItemToPurchase.cpp
    Completed ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,71 @@
-/* Type your code here */
+#include "ItemToPurchase.h"
+#include <iostream>
+
+using namespace std;
+
+//Default Constructor
+ItemToPurchase::ItemToPurchase(){
+  itemName = "none";
+  itemPrice = 0;
+  itemQuantity = 0;
+  itemDescription = "none";
+}
+
+//Constructor
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
+  itemName = name;
+  itemPrice = price;
+  itemQuantity = quantity;
+  itemDescription = description;
+}
+
+//Set Name
+void ItemToPurchase::SetName(string n){
+  itemName = n;
+}
+
+//Get Name
+string ItemToPurchase::GetName(){
+  return itemName;
+}
+
+//Set Price
+void ItemToPurchase::SetPrice(int p){
+  itemPrice = p;
+}
+
+//Get Price
+int ItemToPurchase::GetPrice(){
+  return itemPrice;
+}
+
+//Set Quantity
+void ItemToPurchase::SetQuantity(int q){
+  itemQuantity = q;
+}
+
+//Get Quantity
+int ItemToPurchase::GetQuantity(){
+  return itemQuantity;
+}
+
+//Set Description
+void ItemToPurchase::SetDescription(string d){
+  itemDescription = d;
+}
+
+//Get Description
+string ItemToPurchase::GetDescription(){
+  return itemDescription;
+}
+
+void ItemToPurchase::PrintItemCost(){
+  int subtotal = itemQuantity*itemPrice;
+  cout << itemName << "  " << itemQuantity << " @ $" << itemPrice << " = $" << subtotal << endl;
+}
+
+void ItemToPurchase::PrintItemDescrition(){
+  cout << itemName << ": " << itemDescription << endl;
+}
+
+
