fbdcaaa653011995c79a8afd3c7415d7a83c47fd
rhiltz22 <167123529+rhiltz22@users.noreply.github.com>
2024-04-22 13:49:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,13 @@ using namespace std;
 #include "ShoppingCart.h"
 
 void PrintMenu() {
-   /* Type your code here */
+   cout << "MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output items' descriptions" << endl;
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" <<  endl;
    
 }
 
